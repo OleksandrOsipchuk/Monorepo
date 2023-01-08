2823eacce1ea86a1a487453797345e262b7bc203
OleksandrOsipchuk <oleksandr.osipchuk.r@gmail.com>
2023-01-08 13:00:01 +0200

    interface

---
+++ Osipchuk/IAdditionalOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    interface IAdditionalOperations
+    {
+        int UsingNumber { get; set; }
+        int Operation();
+        int PrintResult();
+    }
+}
