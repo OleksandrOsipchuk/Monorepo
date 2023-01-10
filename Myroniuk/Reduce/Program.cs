b5a24eb5ba96b9b2d82352de6b3aff4b5920ef9f
OleksandrMyroniukUshio <mironiuk76@gmail.com>
2023-01-10 06:44:22 +0100

    rewriting JS prototype.reduce method

---
+++ Myroniuk/Reduce/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReduceApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3] {1,2,3 };
+            int sum = array.Reduce(
+                (accumulator, currentValue) => accumulator + currentValue);
+            Console.WriteLine(sum); // 6
+            
+            string[] array2 = new string[3] { "Hell", "o", " World!" };
+            string result = array2.Reduce(
+                (accumulator, currentValue) => accumulator + currentValue);
+            Console.WriteLine(result); //Hello World!
+
+        }
+    }
+}
---
+++ Myroniuk/Reduce/prototypeReduce.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReduceApplication
+{
+    public delegate T AccumulatorDelegate<T>(T accumulator, T currentValue);
+    public static class prototypeReduce
+    {
+        public static T Reduce<T>(this T[] array, AccumulatorDelegate<T> accumulator)
+        {
+            T accumulatorValue = default(T);
+            foreach(T value in array)
+            {
+                accumulatorValue = accumulator(accumulatorValue, value);
+            }
+            return accumulatorValue;
+        }
+    }
+}
