d4275b5f49e2586e20a4637ee927caf1d053031c
OleksandrMyroniukUshio <mironiuk76@gmail.com>
2023-01-14 17:40:50 +0100

    tried to use IEnumerator instead of array

--- Myroniuk/Reduce/Program.cs
+++ Myroniuk/Reduce/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int[] array = new int[3] {1,2,3 };
+            int[] array = {1,2,3};
             int sum = array.Reduce(
                 (accumulator, currentValue) => accumulator + currentValue);
             Console.WriteLine(sum); // 6
             
-            string[] array2 = new string[3] { "Hell", "o", " World!" };
+            string[] array2 = { "Hell", "o", " World!" };
             string result = array2.Reduce(
                 (accumulator, currentValue) => accumulator + currentValue);
             Console.WriteLine(result); //Hello World!
