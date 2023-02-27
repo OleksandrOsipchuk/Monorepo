89de7ffea3d96925b6b91cbd93a6bcb368ac776e
Liza Snizhko <snizhko.liza@gmail.com>
2023-02-27 11:43:19 -0800

    creating Transaction class

---
+++ Liza/BugdetTracker/BugdetTracker/Transaction.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BugdetTracker
+{
+    public class Transaction
+    {
+        public int currentSum { get; }
+        public int transactionSum { get; set; }
+
+        public Transaction(int currentSum, int transactionSum)
+        {
+            this.currentSum = currentSum;
+            this.transactionSum = transactionSum;
+        }
+    }
+}
