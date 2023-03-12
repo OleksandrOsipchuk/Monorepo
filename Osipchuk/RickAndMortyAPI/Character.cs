2149ab251283f0783682370c48cd9996daa87aa1
OleksandrOsipchuk <oleksandr.osipchuk.r@gmail.com>
2023-03-12 14:00:51 +0200

    new project

---
+++ Osipchuk/RickAndMortyAPI/Character.cs
@@ -0,0 +1,18 @@
+﻿namespace RickMorty
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Type { get; set; }
+        public string Gender { get; set; }
+        public object Origin { get; set; }
+        public object Location { get; set; }
+        public string Image { get; set; }
+        public string[] Episode { get; set; }
+        public string Url { get; set; }
+        public string Created { get; set; }
+    }
+}
